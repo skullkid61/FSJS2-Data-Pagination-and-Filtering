5f9e6ffe1843b1a3e5147991882c0eeba39244f3
Dace Howard <dace61@gmail.com>
2020-10-07 22:38:58 +1030

    created half of walkthrough

--- js/script.js
+++ js/script.js
@@ -11,21 +11,12 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
+const itemsPerPage = 9;
+
 function showStudents(arr) {
    const studentList = document.querySelector('ul.student-list');
    studentList.innerHTML = '';
-   for (i=0;i<arr.length;i++) {
-      studentList.innerHTML += `<li class="student-item cf">
-      <div class="student-details">
-         <img class="avatar" src="${arr[i].picture.large}" alt="Profile Picture">
-         <h3>${arr[i].name.first} ${arr[i].name.last}</h3>
-         <span class="email">${arr[i].email}</span>
-      </div>
-      <div class="joined-details">
-         <span class="date">Joined ${arr[i].registered.date}</span>
-      </div>
-      </li>`
-   }
+   
 }
 showStudents(data);
 
@@ -34,14 +25,26 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
-// function showPage(list, page) {
-//   const studentList = document.querySelector('ul.student-list');
-//   studentList.innerHTML = '';
-//   let startIndex = (page * 9) - 9;
-//   let endIndex = page * 9;
-  
-//    // decide whether to pass through only 9 student at a time or just only display 9 students at a time 
-// }
+function showPage(list, page) {
+  const studentList = document.querySelector('ul.student-list');
+  studentList.innerHTML = '';
+  let startIndex = (page * itemsPerPage) - itemsPerPage;
+  let endIndex = page * itemsPerPage;
+  for (i=0;i<list.length;i++) {
+     if (i >= startIndex && i < endIndex) {
+        studentList.innerHTML += `<li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+               <h3>${list[i].name.first} ${list[i].name.last}</h3>
+               <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+         </li>`
+     }
+   }
+}
 
 /*
 Create the `addPagination` function
